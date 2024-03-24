cd15d8539f6053397d2fe5bb2f2e8a462be16b73
Elton Trujillo <eltontrujillo@outlook.com>
2024-03-23 23:29:16 -0500

    made prob1 expected output

--- prob1.rb
+++ prob1.rb
@@ -1,7 +1,7 @@
 def extract_urls(text)
   #pattern =/(?:http(s?)|ftp):\/\/[\S]+/
   #text.match?(text)
-  pattern = text.match?(/(?:https?|ftp):\/\/[\S]+/)
+  pattern = text.match(/(?:https?|ftp):\/\/[\S]+/)
 end
 
 sample_text = "Visit our site at http://www.example.org for more information. Check out our search page at https://example.com/search?q=ruby+regex. Don’t forget to ftp our resources at ftp://example.com/resources."
